70df2489d040f7104171c901a559a76a4da90765
parfenovaaa <Parfenovaa.aa@yandex.ru>
2021-12-28 15:39:20 +0300

    homework mobile. softAssert in WebMobileTests

--- src/test/java/com/epam/tc/hw/mobile/scenarios/WebMobileTests.java
+++ src/test/java/com/epam/tc/hw/mobile/scenarios/WebMobileTests.java
@@ -1,19 +1,18 @@
 package com.epam.tc.hw.mobile.scenarios;
 
 import com.epam.tc.hw.mobile.pageObjects.WebPageObject;
+import com.epam.tc.hw.mobile.setup.BaseTest;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import com.epam.tc.hw.mobile.setup.BaseTest;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
 
 public class WebMobileTests extends BaseTest {
 
-    SoftAssert softAssert= new SoftAssert();
+
 
     @DataProvider(name = "googleEpamWebTest")
     public Object[][] googleEpamWebTest() {
@@ -35,11 +34,14 @@ public class WebMobileTests extends BaseTest {
 
         webPageObject.searchOnGoogle(keyword);
 
+        SoftAssert softAssert= new SoftAssert();
+
         List<String> actualLinks = webPageObject.getSearchLinks();
-        Assert.assertTrue(actualLinks.contains(expected),
+        softAssert.assertTrue(actualLinks.contains(expected),
                 "failure - There is no link on " + expected + " on  the page.");
 
         List<String> actualNames = webPageObject.getSearchNames();
+
         for (String element: actualNames) {
             softAssert.assertTrue(element.contains(keyword));
         }
